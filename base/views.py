e1cad980fe5e41fd29462991a8c15b177c897009
david <kaneshiuchiha@gmail.com>
2024-03-11 16:24:10 -0500

    search funtionallity

--- base/views.py
+++ base/views.py
@@ -1,43 +1,31 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.<ctrl63> # models.py
 
 
-
-rooms = [
-    {
-        'id': 1,
-        'name': 'Living Room',
-        'description': 'This is the living room. It has a TV, a couch, and a few chairs.'
-    },
-    {
-        'id': 2,
-        'name': 'Bedroom',
-        'description': 'This is the bedroom. It has a bed, a dresser, and a nightstand.'
-    },
-    {
-        'id': 3,
-        'name': 'Kitchen',
-        'description': 'This is the kitchen. It has a stovetop, a sink, and a refrigerator.'
-    }
-]
-
 def home(request):
-    rooms = Room.objects.all()
-    return render(request, 'base/home.html', {'rooms': rooms})
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q) 
+     
+     )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count }
+    return render(request, 'base/home.html', context)
+
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     return render(request, 'base/room.html', {'room': room})
 
-"""def create_room(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        description = request.POST['description']
-        room = Room(name=name, description=description)
-        room.save()
-        return redirect('home')"""
+
 
 def create_room(request):
     form = RoomForm()
